a7bb30f2bbff875610af4e72d4e06969fbf507c6
Liron Bdolah <bdolahliron@gmail.com>
2022-02-21 09:44:06 +0200

    Open-sourced

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
 
     # set size
-    root.geometry("825x325")
+    root.geometry("835x385")
     root.resizable(width=False, height=False)
     root.iconbitmap("assets/icon.ico")
 
@@ -58,9 +58,8 @@ if __name__ == '__main__':
         except:
             messagebox.showinfo('Error', "File not found")
 
-        # gives varriable an input
 
-
+    # gives varriable an input
     def Take_input():
 
         inputs = {}
@@ -103,42 +102,41 @@ if __name__ == '__main__':
 
         messagebox.showinfo('Messege:', "Playlist Created!")
 
-        # interface
 
-        # spotify token
+    # interface
 
-
+    # spotify token
     spotify_header = Label(root, text="Spotify token:", bg='#3b3b3b', fg='white')
     spotify_header.config(font=("arial", 12))
     username = Label(root, text="Username:", bg='#3b3b3b', fg='white')
     username_textbox = Text(root, height=1, width=25)
-    redirect_uri = Label(root, text="Redirect_uri:", bg='#3b3b3b', fg='white')
-    redirect_uri_textbox = Text(root, height=1, width=25)
-    client_id = Label(root, text="Client_id:", bg='#3b3b3b', fg='white')
+    redirect_url = Label(root, text="Redirect url:", bg='#3b3b3b', fg='white')
+    redirect_url_textbox = Text(root, height=1, width=25)
+    client_id = Label(root, text="Client id:", bg='#3b3b3b', fg='white')
     client_id_textbox = Text(root, height=1, width=25)
-    secret_id = Label(root, text="Secret_id:", bg='#3b3b3b', fg='white')
+    secret_id = Label(root, text="Secret id:", bg='#3b3b3b', fg='white')
     secret_id_textbox = Text(root, height=1, width=25)
 
     # playlist filters:
-    playlist_filters = Label(root, text="Playlist_filters:", bg='#3b3b3b', fg='white')
+    playlist_filters = Label(root, text="Playlist filters:", bg='#3b3b3b', fg='white')
     playlist_filters.config(font=("arial", 12))
-    playlist_name = Label(root, text="playlist_name:", bg='#3b3b3b', fg='white')
+    playlist_name = Label(root, text="Playlist name:", bg='#3b3b3b', fg='white')
     playlist_name_textbox = Text(root, height=1, width=25)
-    subreddit_name = Label(root, text="   subreddit_name:", bg='#3b3b3b', fg='white')
+    subreddit_name = Label(root, text="   Subreddit name:", bg='#3b3b3b', fg='white')
     subreddit_name_textbox = Text(root, height=1, width=25)
-    number_of_tracks = Label(root, text="Number_of_tracks:", bg='#3b3b3b', fg='white')
+    number_of_tracks = Label(root, text="Number of tracks:", bg='#3b3b3b', fg='white')
     number_of_tracks_textbox = Text(root, height=1, width=4)
-    date_range = Label(root, text="Date_range:", bg='#3b3b3b', fg='white')
+    date_range = Label(root, text="Date range:", bg='#3b3b3b', fg='white')
     category = Label(root, text="Category:", bg='#3b3b3b', fg='white')
 
-    # date_range_menubar:
+    # date range menubar:
     options = ["hour", "day", "week", "month", "year", "all"]
     var = StringVar()
     var.set(options[0])
     omenu = OptionMenu(root, var, *options)
     omenu.configure(background="white", activeforeground='#fe7012', fg='black')
 
-    # date_range_menubar:
+    # category menubar:
     options_category = ["new", "hot", "top"]
     cat = StringVar()
     cat.set(options_category[0])
@@ -156,7 +154,6 @@ if __name__ == '__main__':
     entryBox = Entry()
 
     # set widgets positions:
-
     spotify_header.grid(row=0, column=2, pady=15)
     username.grid(row=2, column=0, pady=10)
     username_textbox.grid(row=2, column=1)
@@ -164,7 +161,7 @@ if __name__ == '__main__':
     redirect_uri_textbox.grid(row=2, column=3)
     client_id.grid(row=3, column=0, pady=10)
     client_id_textbox.grid(row=3, column=1)
-    secret_id.grid(row=3, column=2, pady=10)
+    secret_id.grid(row=3, column=2, pady=20)
     secret_id_textbox.grid(row=3, column=3)
 
     playlist_filters.grid(row=4, column=2, pady=15)
